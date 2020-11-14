badea390a3a9a60976dc1118802bace975293665
Rooshan <rooshan.ahmed@hotmail.com>
2020-11-15 01:04:45 +0500

    Update header.tsx

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -2,15 +2,28 @@ import React from 'react'
 import { AppBar, Toolbar, makeStyles, createStyles, Theme } from '@material-ui/core'
 import spacex from '../assets/spacex.png'
 
-const useStyles = makeStyles((theme: Theme) => createStyles({}));
+const useStyles = makeStyles((theme: Theme) => createStyles({
+    app:{
+        background: 'none',
+    },
+    root:{
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    appBarWrapper:{
+        width: '80%',
+        margin: '0 auto',
+    },
+}));
 
 const Header = () => {
-    // const classes = useStyles();
+    const classes = useStyles();
     
     return (
-        <div>
-            <AppBar>
-                <Toolbar>
+        <div className={classes.root}>
+            <AppBar className={classes.app} elevation={0}>
+                <Toolbar className={classes.appBarWrapper}>
                     <img src={spacex} alt="spacex" height="100px" width="300px" />
                 </Toolbar>
             </AppBar>
