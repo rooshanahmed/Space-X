ef8525e1af0c91a3db218cf8cf08f502efb45644
Rooshan <rooshan.ahmed@hotmail.com>
2020-11-15 02:01:33 +0500

    Update header.tsx

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,34 +1,42 @@
-import React from 'react'
-import { AppBar, Toolbar, makeStyles, createStyles, Theme } from '@material-ui/core'
-import spacex from '../assets/spacex.png'
+import React from "react";
+import {
+  AppBar,
+  Toolbar,
+  makeStyles,
+  createStyles,
+  Theme,
+} from "@material-ui/core";
+import spacex from "../assets/spacex.png";
 
-const useStyles = makeStyles((theme: Theme) => createStyles({
-    app:{
-        background: 'none',
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    app: {
+      background: "none",
     },
-    root:{
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
+    root: {
+      display: "flex",
+      justifyContent: "center",
+      alignItems: "center",
     },
-    appBarWrapper:{
-        width: '80%',
-        margin: '0 auto',
+    appBarWrapper: {
+      width: "80%",
+      margin: "0 auto",
     },
-}));
+  })
+);
 
 const Header = () => {
-    const classes = useStyles();
-    
-    return (
-        <div className={classes.root}>
-            <AppBar className={classes.app} elevation={0}>
-                <Toolbar className={classes.appBarWrapper}>
-                    <img src={spacex} alt="spacex" height="100px" width="300px" />
-                </Toolbar>
-            </AppBar>
-        </div>
-    )
-}
+  const classes = useStyles();
 
-export default Header
+  return (
+    <div className={classes.root}>
+      <AppBar className={classes.app} elevation={0}>
+        <Toolbar className={classes.appBarWrapper}>
+          <img src={spacex} alt="spacex" height="100px" width="300px" />
+        </Toolbar>
+      </AppBar>
+    </div>
+  );
+};
+
+export default Header;
