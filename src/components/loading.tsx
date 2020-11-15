0d8baaabf1979de33ccc70f1d235846ede30c578
rooshanahmed <rooshan.ahmed@hotmail.com>
2020-11-15 14:32:17 +0500

    Loading css updated

--- src/components/loading.tsx
+++ src/components/loading.tsx
@@ -1,12 +1,13 @@
-import React from 'react';
-import { CircularProgress } from '@material-ui/core';
+import React from "react";
+import { CircularProgress } from "@material-ui/core";
+import "../App.css";
 
-function Loading(){
-    return(
-        <div>
-            <CircularProgress />
-            <h6>Loading...</h6>
-        </div>
-    )
+function Loading() {
+  return (
+    <div className="loading-center">
+      <CircularProgress />
+      <h6>Loading...</h6>
+    </div>
+  );
 }
 export default Loading;
