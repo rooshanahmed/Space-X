56c34c0d01f67ea515c0d089caf3b6eb8fe32876
Rooshan <rooshan.ahmed@hotmail.com>
2020-11-15 13:48:16 +0500

    Src Updated

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,16 @@
 import React from "react";
 import Header from "./components/header/header";
 import "./App.css";
+import { Routes, Route } from 'react-router-dom';
+import LaunchFunction from "./components/Launch/app";
 
 function App() {
   return (
       <div>
         <Header />
+        <Routes>
+          <Route path='/' element={<LaunchFunction />} />
+        </Routes>
       </div>
   );
 }
